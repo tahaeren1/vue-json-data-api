08c319df1f1dbf2cde03c19fdc95f2264bb401e3
ibo <47542461+tahaeren1@users.noreply.github.com>
2021-02-07 18:47:37 +0300

    TodoList olusturuldu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,199 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <div class="row col-md-12">
+      <div class="d-flex justify-content-between input-text">
+      <input placeholder="Lütfen yazı giriniz" v-model="todoText" type="text" class="form-control text-form" aria-label="Text input with segmented dropdown button">
+        <button   @click="addTodo()" v-if="todoText.length > 0" class="btn btn-primary"><i class="fas fa-plus"></i></button>
+      </div>
+      <div class="app">
+
+          <ul class="list-group">
+          <Todo @completeTodo="completeTodo($event)" @deleteTodo="deleteTodo($event)" @returnTodo="returnTodo($event)" :class="{'isCompleted' : todo.isCompleted}"  v-for="todo in todoList" :todo="todo" :key="todo.id "/>
+          </ul>
+        </div>
+      <div class="d-flex flex-row-reverse bd-highlight mt-5">
+      <button class="btn-info"  type="button" @click="allDone()">
+        All Done
+      </button>
+
+      </div>
+    </div>
+
   </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Todo from "@/components/Todo";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Todo},
+  data(){
+    return {
+      todoText : "",
+      todoList : [],
+
+    }
+  },
+  methods : {
+
+
+    addTodo(){
+
+      axios.post("https://vue-deneme-8505d-default-rtdb.firebaseio.com/todoList.json", {text : this.todoText, isCompleted:false})
+      .then(() => {
+        this.getAllTodos()
+
+
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    },
+
+    deleteTodo(todoId){
+      axios.delete("https://vue-deneme-8505d-default-rtdb.firebaseio.com/todoList/" + todoId + ".json")
+      .then(() =>{
+
+       // let index = this.todoList.findIndex(i => {
+       //   return i.id == todoId
+       // })
+       //
+       //  this.todoList.splice(index, 1)
+
+        this.getAllTodos();
+
+      })
+      .catch(e => {
+        console.log(e)
+      });
+    },
+    getAllTodos(){
+     this.todoList = [];
+      axios.get("https://vue-deneme-8505d-default-rtdb.firebaseio.com/todoList.json")
+          .then(response => {
+
+            for (let key in response.data){
+
+              let todo = {
+                text : response.data[key].text,
+                id : key,
+                isCompleted: response.data[key].isCompleted,
+
+              }
+              this.todoList.push(todo);
+
+
+
+            }
+            console.log(this.todoList);
+          })
+
+    },
+    completeTodo(todoId){
+      console.log("test" + todoId);
+      axios.patch("https://vue-deneme-8505d-default-rtdb.firebaseio.com/todoList/" + todoId + ".json", {isCompleted: true})
+          .then(() =>{
+
+            this.getAllTodos();
+
+            let d = new Date();
+
+            console.log(d.getHours());
+
+
+          })
+          .catch(e => {
+            console.log(e)
+          });
+
+    },
+    returnTodo(todoId){
+      console.log("test" + todoId);
+      axios.patch("https://vue-deneme-8505d-default-rtdb.firebaseio.com/todoList/" + todoId + ".json", {isCompleted: false})
+          .then(() =>{
+
+            this.getAllTodos();
+
+
+          })
+          .catch(e => {
+            console.log(e)
+          });
+
+    },
+    allDone(){
+
+      let allData = [];
+      axios.get("https://vue-deneme-8505d-default-rtdb.firebaseio.com/todoList.json")
+          .then(response => {
+            allData = response.data;
+            for (let key in allData) {
+              allData[key].isCompleted = true;
+
+            }
+            console.log(allData)
+            this.updateAllTodo(allData);
+          });
+    },
+
+    updateAllTodo(data) {
+      axios.patch("https://vue-deneme-8505d-default-rtdb.firebaseio.com/todoList.json", data)
+          .then(() =>{
+
+            this.getAllTodos();
+
+
+          })
+          .catch(e => {
+            console.log(e)
+          });
+    }
+
+  },
+
+  created() {
+    this.getAllTodos();
+
+  },
+
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+
+
+.container {
+  background: antiquewhite;
+  width: 100%;
+  height: auto;
+  min-height: 500px;
+  max-width: 500px;
+  min-width: 250px;
+  background-size: 25px 25px;
+  border-radius: 20px;
+  box-shadow: 4px 3px 7px 2px #00000040;
+  padding: 1rem;
+  box-sizing: border-box;
+
 }
+
+.text-form {
+  box-sizing: border-box;
+  background-color: transparent;
+  padding: 0.7rem;
+  border: solid 3px transparent;
+  width: 80%;
+  font-family: 'Gochi Hand', cursive;
+  font-size: 1rem;
+}
+
+
+
 </style>
+
+
